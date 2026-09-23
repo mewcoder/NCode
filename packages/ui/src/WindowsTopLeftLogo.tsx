bf527f5e68e79036cfcdcdfa6eef45a4188c7f40
mewcoder <1010953107@qq.com>
2026-09-23 22:34:54 +0800

    feat(branding): update NCode app icons and logos

--- packages/ui/src/WindowsTopLeftLogo.tsx
+++ packages/ui/src/WindowsTopLeftLogo.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/components/lib/utils.js";
-import zaiLogoUrl from "@/assets/provider-icons/logo-zai.svg";
+import appLogoUrl from "@/assets/app-logo.svg";
 
 export function WindowsTopLeftLogo({
   className,
@@ -20,8 +20,8 @@ export function WindowsTopLeftLogo({
       )}
     >
       <img
-        src={zaiLogoUrl}
-        alt="ZCode"
+        src={appLogoUrl}
+        alt="NCode"
         className={cn("pointer-events-none size-5 select-none", imageClassName)}
         draggable={false}
       />
